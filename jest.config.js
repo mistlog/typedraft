86c4eadcf7f6425206cfa6ad32e1a5dd7387f5f2
mistlog <mistlog@outlook.com>
2021-11-03 22:19:23 +0800

    remove litscript

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  notify: true,
-  coverageDirectory: './docs/coverage'
+    preset: "ts-jest",
+    testEnvironment: "node",
+    notify: true,
+    coverageDirectory: "./coverage",
 };
--- script/clean-docs.ts
+++
@@ -1,8 +0,0 @@
-import { removeSync } from "fs-extra";
-import { resolve } from "path";
-
-const docs_root = resolve(__dirname, "../docs");
-
-["cli", "js", "src", "css", "coverage", "dependencies.json"].forEach(each => {
-    removeSync(resolve(docs_root, each));
-});
